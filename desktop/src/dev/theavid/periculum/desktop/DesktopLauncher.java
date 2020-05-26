460749273e160acbada6f273c44e44e42feeb49e
TheAvidDev <AvidDev@protonmail.com>
2020-05-25 20:49:29 -0400

    Initial commit
     - Add .gitignore
     - Create libGDX project

---
+++ desktop/src/dev/theavid/periculum/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.theavid.periculum.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.theavid.periculum.Periculum;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Periculum(), config);
+	}
+}
