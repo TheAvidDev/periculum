50ec3c2d94fcb6214e8ae7d3a8095d54f2167a9e
TheAvidDev <AvidDev@protonmail.com>
2020-05-25 21:18:59 -0400

    Add existing assets

---
+++ core/assets/tiles/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="Base" tilewidth="16" tileheight="16" tilecount="7800" columns="16">
+ <image source="tilemap.png" width="256" height="256"/>
+</tileset>
