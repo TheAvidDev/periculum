65f2f8fcf5929dcf288d53e126111d4d90bb4fa9
Diana Lin <diana4lin@gmail.com>
2020-05-28 00:14:43 -0400

    Add Tiled integration for level drawing

--- core/src/dev/theavid/periculum/Periculum.java
+++ core/src/dev/theavid/periculum/Periculum.java
@@ -6,28 +6,39 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * Main game class 
+ * 
+ * @author hirundinidae
+ */
+// 2020-05-27 hirundinidae - Called Level.java methods
 public class Periculum extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    SpriteBatch batch;
+    Texture img;
+    Level level;
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        level = new Level();
+        level.create();
+    }
 
-	@Override
-	public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        level.render();
+        batch.end();
 
-	@Override
-	public void dispose() {
-		batch.dispose();
-		img.dispose();
-	}
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+        level.dispose();
+    }
 }
