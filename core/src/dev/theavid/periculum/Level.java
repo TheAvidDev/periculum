eefc27739b9449c160c4ac7ad5326781c6d29625
Diana Lin <diana4lin@gmail.com>
2020-05-29 00:07:04 -0400

    Added camera and its movement based on the Player

--- core/src/dev/theavid/periculum/Level.java
+++ core/src/dev/theavid/periculum/Level.java
@@ -1,31 +1,33 @@
 package dev.theavid.periculum;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-/** 
- * Draws the map of the level. 
+/**
+ * Draws the map of the level.
  * 
  * @author hirundinidae
  */
+// 2020-05-28 hirundinidae - removed unused interface, started using camera  
 // 2020-05-27 hirundinidae - Tiled integration to libGDX, level drawing 
-public class Level extends ApplicationAdapter {
+public class Level {
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
-    @Override
     public void create() {
         map = new TmxMapLoader().load("tiles/map.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
     }
 
-    public void render() {
+    public void render(OrthographicCamera camera) {
+        renderer.setView(camera);
         renderer.render();
     }
 
     public void dispose() {
         map.dispose();
     }
+
 }
--- core/src/dev/theavid/periculum/Periculum.java
+++ core/src/dev/theavid/periculum/Periculum.java
@@ -3,6 +3,7 @@ package dev.theavid.periculum;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
@@ -11,37 +12,45 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  * @author hirundinidae
  * @author TheAvidDev
  */
+// 2020-05-28 hirundinidae - created camera and its movement based on player
 // 2020-05-28 TheAvidDev - cleaned up code, removed libGDX defaults
 // 2020-05-27 hirundinidae - added level creation and rendering
 public class Periculum extends ApplicationAdapter {
-	SpriteBatch batch;
-	Player player;
-	Level level;
+    SpriteBatch batch;
+    Player player;
+    Level level;
+    OrthographicCamera camera;
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		player = new Player();
-		level = new Level();
-		level.create();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        player = new Player();
+        level = new Level();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 320, 320 * (800 / 480));
+        level.create();
+    }
 
-	@Override
-	public void render() {
-		player.update();
+    @Override
+    public void render() {
+        player.update();
+        camera.position.set(player.getX(), player.getY(), 0);
+        camera.update();
 
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		level.render();
-		batch.begin();
-		batch.draw(player.getTextureRegion(), player.getX(), player.getY());
-		batch.end();
-	}
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        level.render(camera);
 
-	@Override
-	public void dispose() {
-		batch.dispose();
-		player.dispose();
-		level.dispose();
-	}
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(player.getTextureRegion(), player.getX(), player.getY());
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        player.dispose();
+        level.dispose();
+    }
 }
