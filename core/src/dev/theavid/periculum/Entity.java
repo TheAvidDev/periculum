d664480fb33cda2f5ac8a13494cd833aef821cb4
Diana Lin <diana4lin@gmail.com>
2020-05-30 01:47:58 -0400

    Add entity and entity types

---
+++ core/src/dev/theavid/periculum/Entity.java
@@ -0,0 +1,12 @@
+package dev.theavid.periculum;
+
+/**
+ * Entity class 
+ * 
+ * @author Diana
+ */
+// 2020-05-29 hirundinidae - Created Entitiy class.
+public class Entity {
+    private double x, y; 
+    EntityType entityType; 
+}
