d515592ec6e66f5dcab40a931dde1c9d01e2e166
TheAvidDev <AvidDev@protonmail.com>
2020-05-29 22:49:30 -0400

    Add collision debugging

--- core/src/dev/theavid/periculum/Debugger.java
+++ core/src/dev/theavid/periculum/Debugger.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 /**
  * A debugging class to help with fixing problems and finding bugs.
@@ -52,6 +54,7 @@ public class Debugger {
     	shapeRenderer.setProjectionMatrix(camera.combined);
     	shapeRenderer.begin(ShapeType.Filled);
 		debugLevelCollisions();
+		debugMouse();
     	shapeRenderer.end();
         Gdx.gl.glDisable(GL20.GL_BLEND);
 
@@ -70,17 +73,72 @@ public class Debugger {
 	}
 	
 	/**
-	 * Draw a partially transparent red rectangle over collidable tiles.
+	 * Draw filled rectangles around where the mouse is in relation to the
+	 * level. Two separate rectangles are drawn: one for the normal, aligned
+	 * tiles, and another for the offset tiles such as the roof.
+	 */
+	private void debugMouse() {
+	    Vector3 mousePos = new Vector3();
+	    mousePos.x = Gdx.input.getX();
+	    mousePos.y = Gdx.input.getY();
+	    mousePos.z = 0;
+	    camera.unproject(mousePos);
+
+		shapeRenderer.setColor(0, 0, 0, 0.5f);
+		shapeRenderer.rect((int) (mousePos.x/16) * 16, (int) ((mousePos.y - 11)/16) * 16 + 11, 16, 16);
+		shapeRenderer.setColor(1, 1, 1, 0.5f);
+		shapeRenderer.rect((int) (mousePos.x/16) * 16, (int) (mousePos.y/16) * 16, 16, 16);
+	}
+
+	/**
+	 * Draw rectangles related to collision detection. The colors are
+	 * described below:
+	 *  - Blue: a tile which has collision
+	 *  - Red: a tile which is being checked for collision
+	 *  - Green: a tile which is being collided with (inaccurate*)
+	 *  - White: the player's actual collision detection box
+	 *  - Black: an extra pixel around each side of the player's collision
+	 *           box to simulate where a collision occurred the previous
+	 *           frame
+	 *
+	 * [*] This is inaccurate because the player will attempt to move into a
+	 *     wall and get pushed out in the player's update method so knowing
+	 *     for sure what a player collided with is impossible unless debugging
+	 *     is drawn there.
 	 */
 	private void debugLevelCollisions() {
-    	shapeRenderer.setColor(1, 0, 0, 0.5f);
+        shapeRenderer.setColor(0, 0, 1, 0.5f);
     	for (int x = 0; x < level.getCollisionMap().length; x ++) {
         	for (int y = 0; y < level.getCollisionMap()[0].length; y ++) {
         		if (level.getCollisionMap()[x][y]) {
         			shapeRenderer.rect(x*16, y*16 + 11, 16, 16);
-        		}
-        	}	
+                }
+            }
         }
+    	
+    	/**
+    	 * Read the [*] above for why this is offset by a pixel.
+    	 */
+    	Rectangle playerRect = new Rectangle(player.getX(), player.getY()-1, 16, 18);
+    	int xMap = (int) (player.getX() / 16);
+    	int yMap = (int) ((player.getY() - 11) / 16);
+    	for (int xm = xMap-1; xm <= xMap+1; xm ++) {
+    		for (int ym = yMap-1; ym <= yMap+1; ym ++) {
+    			Rectangle collisionRect = new Rectangle(xm*16, ym*16+11, 16, 16);
+    			if (level.getCollisionMap()[xm][ym] && collisionRect.overlaps(playerRect)) {
+    				shapeRenderer.setColor(0, 1, 0, 0.5f);
+    			} else {
+    				shapeRenderer.setColor(1, 0, 0, 0.5f);
+    			}
+    			if (level.getCollisionMap()[xm][ym]) {
+    				shapeRenderer.rect(collisionRect.x, collisionRect.y, collisionRect.width, collisionRect.height);
+    			}
+        	}
+    	}
+    	shapeRenderer.setColor(0, 0, 0, 0.5f);
+    	shapeRenderer.rect(playerRect.x, playerRect.y, playerRect.width, playerRect.height);
+    	shapeRenderer.setColor(1, 1, 1, 0.5f);
+    	shapeRenderer.rect(playerRect.x+1, playerRect.y+1, playerRect.width-2, playerRect.height-2);
 	}
 	
 	/**
@@ -90,7 +148,9 @@ public class Debugger {
 		String debug = "X: " + player.getX() + "\n"
 			+ "Y: " + player.getY() + "\n"
 			+ "XVel: " + player.getXVel() + "\n"
-			+ "YVel: " + player.getYVel();
+			+ "YVel: " + player.getYVel() + "\n"
+			+ "CYVel: " + Math.ceil(player.getYVel()) + "\n"
+			+ "FYVel: " + Math.floor(player.getYVel());
 		font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
 		font.draw(batch, debug, 10, camera.viewportHeight*2-40);
 	}
