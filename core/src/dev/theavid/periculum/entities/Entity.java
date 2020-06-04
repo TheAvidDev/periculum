69ff69508b6b4cf034047d5319906419ccd9382a
TheAvidDev <AvidDev@protonmail.com>
2020-06-04 03:32:23 -0400

    Update entities code and add notififer entity

--- core/src/dev/theavid/periculum/entities/Entity.java
+++ core/src/dev/theavid/periculum/entities/Entity.java
@@ -1,12 +1,40 @@
 package dev.theavid.periculum.entities;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 /**
- * Entity class 
+ * An entity is any object that can be drawn and has some position in game but
+ * is not part of the background tiles. This include objects like the player,
+ * notifiers, and other characters.
  * 
- * @author Diana
+ * @author TheAvidDev
+ * @author hirundinidae
  */
-// 2020-05-29 hirundinidae - Created Entitiy class.
+// 2020-06-03 TheAvidDev - Add methods to help with drawing and interface with
+//                         the EntityType
+// 2020-05-29 hirundinidae - Created Entitiy class
 public class Entity {
-    private double x, y; 
-    EntityType entityType; 
+    private float x, y;
+    EntityType entityType;
+    
+    public Entity(EntityType entityType, float x, float y) {
+      this.entityType = entityType;
+      this.x = x;
+      this.y = y;
+    }
+    
+    public void update() {
+      return;
+    }
+    
+    public void dispose() {
+      entityType.dispose();
+    }
+    
+    public TextureRegion getTextureRegion() {
+      return entityType.getTextureRegion();
+    }
+    
+    public float getX() { return x; }
+  public float getY() { return y; }
 }
--- core/src/dev/theavid/periculum/entities/EntityType.java
+++ core/src/dev/theavid/periculum/entities/EntityType.java
@@ -1,33 +1,46 @@
 package dev.theavid.periculum.entities;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
  * An enum for non static entities. 
  * 
- * @author Diana
+ * @author TheAvidDev
+ * @author hirundinidae
  */
-// 2020-05-29 hirundinidae - Created enum 
+// 2020-06-03 TheAvidDev - Added more getter methods and loaded textures
+// 2020-05-29 hirundinidae - Created enum
 public enum EntityType {
+    NOTIFIER("notifier.png", false, 16, 16);
 
-    ;
-    
-    private final String fileName;
+    private final String BASE_DIR = "entities/";
+  
     private final boolean collidable;
     private final int width;
     private final int height;
-    private Texture texture;
+    private TextureRegion[][] textureRegions;
 
-    EntityType(String fileName, boolean collidable, int width, int height, Texture texture) {
-        this.fileName = fileName; 
-        this.collidable=collidable; 
-        this.width = width; 
-        this.height = height; 
-        this.texture = texture; 
+    EntityType(String filename, boolean collidable, int width, int height) {
+        this.collidable = collidable;
+        this.width = width;
+        this.height = height;
+        textureRegions = TextureRegion.split(new Texture(BASE_DIR + filename), width, height);
     }
 
-    public Texture getTexture() {
-        return texture;
+    public TextureRegion getTextureRegion(int row, int column) {
+        return textureRegions[row][column];
     }
 
+    public TextureRegion getTextureRegion() {
+        return textureRegions[0][0];
+    }
+    
+    public void dispose() {
+        textureRegions[0][0].getTexture().dispose();
+    }
+    
+    public boolean getCollidable() { return collidable; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
 }
