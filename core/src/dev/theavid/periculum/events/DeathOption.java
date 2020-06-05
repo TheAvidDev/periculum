f33d2171bbb5df2908c3d542c58793a9b639f6e3
TheAvidDev <AvidDev@protonmail.com>
2020-06-05 11:31:10 -0400

    Add an event system

---
+++ core/src/dev/theavid/periculum/events/DeathOption.java
@@ -0,0 +1,37 @@
+package dev.theavid.periculum.events;
+
+/**
+ * An option which causes a game ending if picked by the player.
+ * 
+ * @author TheAvidDev
+ */
+// 2020-06-05 TheAviDev - Created DeathOption class
+public class DeathOption extends Option {
+	private String deathMessage;
+	private String deathIconFilename;
+
+	public DeathOption(String name, float infectionRateEffect, float mentalStabilityEffect) {
+		super(name, infectionRateEffect, mentalStabilityEffect);
+	}
+
+	/**
+	 * Creates a regular Option with two extra parameters needed for a death screen.
+	 * 
+	 * @param deathMessage the message displayed to the user on death
+	 * @param deathIconFilename the name of the file housing the icon for the death screen
+	 */
+	public DeathOption(String name, float infectionRateEffect, float mentalStabilityEffect, String deathMessage,
+			String deathIconFilename) {
+		this(name, infectionRateEffect, mentalStabilityEffect);
+		this.deathMessage = deathMessage;
+		this.deathIconFilename = deathIconFilename;
+	}
+
+	public String getDeathMessage() {
+		return deathMessage;
+	}
+
+	public String getDeathIconFilename() {
+		return deathIconFilename;
+	}
+}
---
+++ core/src/dev/theavid/periculum/events/Option.java
@@ -0,0 +1,38 @@
+package dev.theavid.periculum.events;
+
+/**
+ * 
+ * 
+ * @author TheAvidDev
+ */
+// 2020-06-05 TheAvidDev - Created Option class
+public class Option {
+	private String name;
+	private float infectionRateEffect;
+	private float mentalStabilityEffect;
+
+	/**
+	 * Creates a new option for an Event.
+	 * 
+	 * @param name the name of this option
+	 * @param infectionRateEffect the effect this option has on infection risk
+	 * @param mentalStabilityEffect the effect this option has on mental stability
+	 */
+	public Option(String name, float infectionRateEffect, float mentalStabilityEffect) {
+		this.name = name;
+		this.infectionRateEffect = infectionRateEffect;
+		this.mentalStabilityEffect = mentalStabilityEffect;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public float getInfectionRateEffect() {
+		return infectionRateEffect;
+	}
+
+	public float getMentalStabilityEffect() {
+		return mentalStabilityEffect;
+	}
+}
