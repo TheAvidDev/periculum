875118de5288efd095701ab554b8dfd9d6f99fcb
TheAvidDev <AvidDev@protonmail.com>
2020-06-05 12:16:59 -0400

    Fix NullPointerException on transition

--- core/src/dev/theavid/periculum/Periculum.java
+++ core/src/dev/theavid/periculum/Periculum.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 import dev.theavid.periculum.gamestates.GameState;
-import dev.theavid.periculum.gamestates.PlayingGameState;
 import dev.theavid.periculum.gamestates.SplashGameState;
 
 /**
@@ -21,70 +20,71 @@ import dev.theavid.periculum.gamestates.SplashGameState;
 // 2020-06-04 hirundinidae - Updated comment for game state transitions  
 // 2020-05-30 TheAvidDev - Switched to a game state based approach
 public class Periculum extends ApplicationAdapter {
-    private final int TILE_WIDTH = 16;
-    private final int VIEWPORT_WIDTH = 16;
+	private final int TILE_WIDTH = 16;
+	private final int VIEWPORT_WIDTH = 16;
 
-    private OrthographicCamera camera;
-    private ShapeRenderer shapeRenderer;
-    private GameState currentGameState;
-    private float transitionCounter = -1;
+	private OrthographicCamera camera;
+	private ShapeRenderer shapeRenderer;
+	private GameState currentGameState;
+	private float transitionCounter = -1;
 
-    @Override
-    public void create() {
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 320, 320 * (4 / 3));
-        shapeRenderer = new ShapeRenderer();
-        currentGameState = new SplashGameState(camera);
-    }
+	@Override
+	public void create() {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 320, 320 * (4 / 3));
+		shapeRenderer = new ShapeRenderer();
+		currentGameState = new SplashGameState(camera);
+	}
 
-    @Override
-    public void render() {
-        currentGameState.update();
-        currentGameState.render();
+	@Override
+	public void render() {
+		currentGameState.update();
+		currentGameState.render();
 
-        /**
-         * Update the transition counter and transition to the playing game state if the
-         * SPACE key is pressed.
-         */
-        if (transitionCounter != 0 || currentGameState.shouldTransistion()) {
-            transitionCounter += Gdx.graphics.getDeltaTime();
-            Gdx.gl.glEnable(GL20.GL_BLEND);
-            Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-            shapeRenderer.begin(ShapeType.Filled);
-            shapeRenderer.setColor(0, 0, 0, Math.abs(transitionCounter));
-            shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            shapeRenderer.end();
-            Gdx.gl.glDisable(GL20.GL_BLEND);
-            if (transitionCounter < 0 && transitionCounter + Gdx.graphics.getDeltaTime() > 0) {
-                transitionCounter = 0;
-            }
-        }
-        if (transitionCounter >= 1) {
-            currentGameState = currentGameState.getNextGameState();
-        }
-    }
+		/**
+		 * Update the transition counter and transition to the playing game state if the
+		 * SPACE key is pressed.
+		 */
+		if (transitionCounter != 0 || currentGameState.shouldTransistion()) {
+			transitionCounter += Gdx.graphics.getDeltaTime();
+			Gdx.gl.glEnable(GL20.GL_BLEND);
+			Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+			shapeRenderer.begin(ShapeType.Filled);
+			shapeRenderer.setColor(0, 0, 0, Math.abs(transitionCounter));
+			shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			shapeRenderer.end();
+			Gdx.gl.glDisable(GL20.GL_BLEND);
+			if (transitionCounter < 0 && transitionCounter + Gdx.graphics.getDeltaTime() > 0) {
+				transitionCounter = 0;
+			}
+		}
+		if (transitionCounter >= 1) {
+			transitionCounter = -1;
+			currentGameState = currentGameState.getNextGameState();
+		}
+	}
 
-    @Override
-    public void dispose() {
-        currentGameState.dispose();
-    }
+	@Override
+	public void dispose() {
+		currentGameState.dispose();
+	}
 
-    /**
-     * This method resizes the camera's viewport while maintaining scale to allow
-     * any window size. It will base the scale off of the shortest side length, so
-     * ultra wide screens won't be unnecessarily zoomed in.
-     */
-    @Override
-    public void resize(int width, int height) {
-        if ((float) width / (float) height > (float) height / (float) width) {
-            float scale = (float) width / (float) height;
-            camera.viewportWidth = TILE_WIDTH * VIEWPORT_WIDTH * scale;
-            camera.viewportHeight = TILE_WIDTH * VIEWPORT_WIDTH;
-        } else {
-            float scale = (float) height / (float) width;
-            camera.viewportWidth = TILE_WIDTH * VIEWPORT_WIDTH;
-            camera.viewportHeight = TILE_WIDTH * VIEWPORT_WIDTH * scale;
-        }
-        camera.update();
-    }
+	/**
+	 * This method resizes the camera's viewport while maintaining scale to allow
+	 * any window size. It will base the scale off of the shortest side length, so
+	 * ultra wide screens won't be unnecessarily zoomed in.
+	 */
+	@Override
+	public void resize(int width, int height) {
+		if ((float) width / (float) height > (float) height / (float) width) {
+			float scale = (float) width / (float) height;
+			camera.viewportWidth = TILE_WIDTH * VIEWPORT_WIDTH * scale;
+			camera.viewportHeight = TILE_WIDTH * VIEWPORT_WIDTH;
+		} else {
+			float scale = (float) height / (float) width;
+			camera.viewportWidth = TILE_WIDTH * VIEWPORT_WIDTH;
+			camera.viewportHeight = TILE_WIDTH * VIEWPORT_WIDTH * scale;
+		}
+		camera.update();
+	}
 }
